d1a4b5d51b7400364c8713cf2aacec9d804dd3ec
Wnstjrdl <wnstjr490@naver.com>
2022-05-26 22:46:45 +0900

    틀 재구성

--- src/main/java/com/its/member/controller/MemberController.java
+++ src/main/java/com/its/member/controller/MemberController.java
@@ -1,16 +1,38 @@
 package com.its.member.controller;
 
+import com.its.member.dto.MemberDTO;
 import com.its.member.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/member")
 @Controller
 public class MemberController {
  @Autowired
   private MemberService memberService;
+
     @GetMapping("save")
+    //회원가입 화면
      public String saveForm(){return "memberPages/save";}
+
+    @PostMapping("save")
+    //회원가입 처리
+    public  String save(@RequestParam MemberDTO memberDTO){
+        boolean saveResult = memberService.save(memberDTO);
+        if(saveResult){
+            return  "login";
+        }else{
+            return  "savefail";
+        }
+    }
+
+
+
+
+
 }
+
--- src/main/java/com/its/member/dto/MemberDTO.java
+++ src/main/java/com/its/member/dto/MemberDTO.java
@@ -1,4 +1,17 @@
 package com.its.member.dto;
 
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
 public class MemberDTO {
+   private Long id;
+   private String memberId;
+   private String memberPassword;
+   private String memberName;
+   private String memberEmail;
+   private String memberMobile;
+   private MultipartFile boardFile;
+   private String boardFileName;
+
 }
